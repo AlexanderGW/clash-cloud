859e632727992567a3b21fe20e0c16e42d778dce
AlexanderGW <alex@gailey-white.com>
2019-02-04 23:40:04 +0000

    Add player history route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +13,16 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 Route::get('/clan/{tag}', 'ClanController@index')->name('clan')->where('tag', '[0-9A-Z]+');
 Route::get('/player/{tag}', 'PlayerController@index')->name('player')->where('tag', '[0-9A-Z]+');
+Route::get('/player/{tag}/history', 'PlayerController@history')->name('player.history')->where('tag', '[0-9A-Z]+');
 
-Route::get('/clan/{tag}/{date}', 'ClanController@index')->name('clan')->where('tag', '[0-9A-Z]+');
-Route::get('/player/{tag}/{date}', 'PlayerController@index')->name('player')->where('tag', '[0-9A-Z]+');
+Route::get('/clan/{tag}/{date}', 'ClanController@index')->name('clan.date')->where('tag', '[0-9A-Z]+')->where('date', '([0-9]{4}-[0-9]{2}-[0-9]{2})+');
+//Route::get('/player/{tag}/{date}', 'PlayerController@index')->name('player')->where('tag', '[0-9A-Z]+')->where('date', '([0-9]{4}-[0-9]{2}-[0-9]{2})+');
 
-Route::get('/top/{iso}/clans', 'TopController@clans')->name('top.clans')->where('iso', '[a-z]+');
-Route::get('/top/{iso}/players', 'TopController@players')->name('top.players')->where('iso', '[a-z]+');
+Route::get('/top/{iso}/clans', 'TopController@clans')->name('top.clans')->where('iso', '(international|[a-z]{2})+');
+Route::get('/top/{iso}/players', 'TopController@players')->name('top.players')->where('iso', '(international|[a-z]{2})+');

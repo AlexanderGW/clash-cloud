8bd9f519b1483b35ff4f405af5b86036496e52c3
AlexanderGW <alex@gailey-white.com>
2019-02-04 23:42:21 +0000

    Initial player list component

---
+++ resources/assets/js/components/PlayerList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class PlayerList extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			date: props.date
+		};
+	}
+
+	render() {
+		return(
+			<div>gregr</div>
+		)
+	}
+}

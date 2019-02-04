2a48691a2bb1b0a7d5fdb6d9780e61ce93b2eec1
AlexanderGW <alex@gailey-white.com>
2019-02-04 23:41:59 +0000

    Initial clan list component

---
+++ resources/assets/js/components/ClanList.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class ClanList extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			date: props.date
+		};
+	}
+
+	render() {
+		return(
+			<div>aasaa</div>
+		)
+	}
+}

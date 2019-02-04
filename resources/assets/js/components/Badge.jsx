9849a23d675e6989edc67c31730223173418cfe8
AlexanderGW <alex@gailey-white.com>
2019-02-04 23:43:20 +0000

    Add size prop to badge component

--- resources/assets/js/components/Badge.jsx
+++ resources/assets/js/components/Badge.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
 export default class Badge extends React.Component {
+	getSize(size){
+		//size *= 2;
+		if(size <= 64) {
+			size = 64;
+		} else if(size <= 128) {
+			size = 128;
+		} else if(size <= 256) {
+			size = 256;
+		} else {
+			size = 512;
+		}
+		return size;
+	}
+
 	render(){
 		return(
-			<div><img src={`https://clash.cloud/asset/image/badge/${this.props.name}/128.png`}/></div>
+			<div><img width={this.props.size} src={`https://clash.cloud/asset/image/badge/${this.props.name}/${this.getSize(this.props.size)}.png`}/></div>
 		);
 	}
 }

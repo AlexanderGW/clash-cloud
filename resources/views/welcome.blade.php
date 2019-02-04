55201b46ffaefa836ec9339b2290d04e86e2bf23
AlexanderGW <alex@gailey-white.com>
2019-02-04 23:36:41 +0000

    Add endpoint URIs

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Clash Cloud') }}</title>
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <script>
             window.Laravel = {!! json_encode([
             'csrfToken' => csrf_token(),
+            'appUrl' => env('APP_URL'),
+            'apiUrl' => env('APP_URL') . '/api'
         ]) !!};
         </script>
     </head>
